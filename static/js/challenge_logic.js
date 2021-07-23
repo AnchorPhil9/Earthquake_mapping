40d5e21547ff54b14e69852dc3d1c8fc681da3c6
AnchorPhil9 <83145748+AnchorPhil9@users.noreply.github.com>
2021-07-22 22:26:19 -0400

    Completed most code for Deliverable 2.
    Filled out the d3 call for major earthquakes data after refactoring starter code. Added shades of purple and magenta for each major earthquake point.
    Formatting for Legend is WIP.

--- static/js/challenge_logic.js
+++ static/js/challenge_logic.js
@@ -16,7 +16,7 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 });
 
 // Create the map object with center, zoom level and default layer.
-// Instead of 'let map', we'll use 'var myMap'.
+//// Instead of 'let map', we'll use 'var myMap'.
 var myMap = L.map('mapid', {
 	center: [40.7, -94.5],
 	zoom: 3,
@@ -31,23 +31,23 @@ let baseMaps = {
 
 // 1. Add a 2nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
-// Following 13.6.4 (2021), we'll make a new layer group called 'allTectonicPlates'.
+//// Following 13.6.4 (2021), we'll make a new layer group called 'allTectonicPlates'.
 let allTectonicPlates = new L.LayerGroup();
-// For Deliverable 2 of the Module Challenge 13 (2021), we'll add a third layer group called
-// 'majorEarthQuakes'.
+//// For Deliverable 2 of the Module Challenge 13 (2021), we'll add a third layer group called
+//// 'majorEarthQuakes'.
 let majorEarthQuakes = new L.LayerGroup();
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
   "Earthquakes": allEarthquakes
 };
-// Like in 13.6.4 (2021), we'll make an overlay object called 'tectonics' to 
-// reference one of our layer groups, specifically allTectonicPlates.
+//// Like in 13.6.4 (2021), we'll make an overlay object called 'tectonics' to 
+//// reference one of our layer groups, specifically allTectonicPlates.
 let tectonics = {
   "Tectonic Plates": allTectonicPlates
 };
-// We'll do the same for our 'majorEarthquakes' layer group, calling the
-// overlay object 'majors'
+//// We'll do the same for our 'majorEarthquakes' layer group, calling the
+//// overlay object 'majors'.
 let majors = {
   "Major Earthquakes": majorEarthQuakes
 }
@@ -122,20 +122,76 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // Then we add the earthquake layer to our map.
   allEarthquakes.addTo(myMap);
 
-// To retrieve JSON data of major earthquakes, we'll need d3.json(),
-// as mentioned in the Module 13 Challenge (2021) instructions.
-// For convenience, we'll store the major earthquakes URL in a variable.
+//// Deliverable 2: To retrieve JSON data of major earthquakes, we'll need d3.json(),
+//// as mentioned in the Module 13 Challenge (2021) instructions.
+//// For convenience, we'll store the major earthquakes URL in a variable.
 let mEQ = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson'
-// For our function, we'll use 'major' as our parameter.
+//// For this d3 function, we'll use 'major' as our parameter for major earthquakes.
 d3.json(mEQ).then(function(major) {
-  // To check our progress, we use console.log on our 'major' parameter.
-  console.log(major);
-  // Like we did in Deliverable 1, we'll add the 'major' data
-  // to our 'majorEarthquakes' layer group.
-  L.geoJson(major).addTo(majorEarthQuakes);
-  // Now, we'll add the layer group to the map
+  //// Deliverable 2: We'll refactor the styleInfo code used in prior
+  //// GeoJSON code so that the major earthquakes gets the same style.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+  //// Deliverable 2: Here, we'll refactor the getColor code. This time,
+  //// however, there will be 3 colors for 3 magnitudes: less than
+  //// magnitude 5, greater than magnitude 5, and greater than magnitude 6
+  //// ("Module 13 Challenge", 2021).
+  function getColor(magnitude) {
+    //// Magnitude 5- Earthquakes will be light magenta.
+    if (magnitude <= 5) {
+      return "#ffbbff";
+    }
+    //// Magnitude 5+ Earthquakes will be normal magenta.
+    if (magnitude > 5) {
+      return "#ff00ff";
+    }
+    //// Magnitude 6+ Earthquakes will be purple.
+    if (magnitude > 6) {
+      return "#800080";
+    }
+    //// The rest of the earthquakes will be
+    return "#ffffff";
+  }
+
+  //// Deliverable 2: Then we'll refactor the getRadius code
+  //// ("Module 13 Challenge", 2021).
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+
+  //// Deliverable 2: Since we use 'major' as our parameter for the major  
+  //// earthquakes data, we'll use 'major' here to pass the data into a 
+  //// new geoJSON layer ("Module 13 Challenge", 2021).
+  L.geoJson(major, {
+    pointToLayer: function(feature, latlng) {
+        console.log(major);
+        return L.circleMarker(latlng);
+      },
+  //// As in the starter code, the below code gives each circleMarker
+  //// the style we defined in our styleInfo function ("Module 13 Challenge", 2021).
+  style: styleInfo,
+   //// Per the starter code, we'll use 'bindPopup' to make popups for each circleMarker
+   //// that describe each earthquake's magnitude and location ("Module 13 Challenge", 2021)
+   onEachFeature: function(feature, layer) {
+    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+  }
+  //// Next, we'll add this geoJSON layer to the 'majorEarthQuakes' layer.
+  }).addTo(majorEarthQuakes);
+  //// Finally, we'll add the layer to 'myMap'.
   majorEarthQuakes.addTo(myMap)
-})
+});
 
   // Here we create a legend control object.
 let legend = L.control({
@@ -170,18 +226,18 @@ legend.onAdd = function() {
   legend.addTo(myMap);
 
 
-  // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  // First, per 13.5.3 (2021), we'll define a variable that houses an external URL to the tectonic plate data. 
-  // In particular, we'll dig into the boundaries JSON file.
+  //// 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
+  //// First, per 13.5.3 (2021), we'll define a variable that houses an external URL to the tectonic plate data. 
+  //// In particular, we'll dig into the boundaries JSON file.
   let tectPlateURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
-  // In our d3.json function, we will now make a new function, with a 'tecto' parameter, that uses 
-  // Leaflet's geoJSON() function. For debugging, we'll include console.log inside the tecto function.
+  //// In our d3.json function, we will now make a new function, with a 'tecto' parameter, that uses 
+  //// Leaflet's geoJSON() function. For debugging, we'll include console.log inside the tecto function.
   d3.json(tectPlateURL).then(function(tecto) {
     console.log(tecto);
-    // Per Module 13 Challenge instructions (2021), we'll add the tecto data 
-    // to our 'allTectonicPlates' layer group
+    //// Per Module 13 Challenge instructions (2021), we'll add the tecto data 
+    //// to our 'allTectonicPlates' layer group
     L.geoJson(tecto).addTo(allTectonicPlates);
-    // Finally, we'll add the layer group to the map.
+    //// Finally, we'll add the layer group to the map.
     allTectonicPlates.addTo(myMap)
   });
 });
